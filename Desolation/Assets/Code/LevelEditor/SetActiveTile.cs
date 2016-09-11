101f93011eb9ebaf9dc7c9da8a6152cf1db8667c
Adam Kocić <falkan333@gmail.com>
2016-09-11 18:06:45 +0200

    no message

---
+++ Desolation/Assets/Code/LevelEditor/SetActiveTile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetActiveTile : MonoBehaviour {
+
+    public int index;
+    public byte list; // 0 = floor, 1 = walls, 2 = misc
+    public GameObject tile;
+    private Button button;
+
+    public void setTile()
+    {
+        GameObject levelmanager = GameObject.Find("Level1Manager");
+        TileEditor editor = levelmanager.GetComponent<TileEditor>();
+        editor.ChangeActiveTile(index, tile);
+
+    }
+
+    public void changeSelf(GameObject obj)
+    {
+        tile = obj;
+        button = GetComponent<Button>();
+        button.image.sprite = obj.GetComponent<SpriteRenderer>().sprite;
+    }
+
+}
--- Desolation/Assets/Code/LevelEditor/TileEditor.cs
+++ Desolation/Assets/Code/LevelEditor/TileEditor.cs
@@ -12,12 +12,26 @@ public class TileEditor : MonoBehaviour {
     public GameObject editorTexture;
     private SpriteRenderer editorTextureRenderer;
     private Vector2 ray;
+
     private List<Level.tile> maintiles = new List<Level.tile>();
     private List<Level.tile> misctiles = new List<Level.tile>();
 
-    private GameObject activeObject;
+    //Editor buttons
+    private List<GameObject> listOfFloorButtons;
+    private int floorTileIndex = 0; //index for iterating through maintile list
+
+    private int activeObjectIndex;
+    private GameObject activeObject; //active tile to place
     private GameObject objtodestroy;
-    private int index;
+
+    
+    private int index = 0; //index of tile to destroy
+    //for left tile menu
+    private Vector3 panelPosition;
+    private int editorTileCount;
+    private GameObject newTile;
+    public GameObject menutile;
+    List<GameObject> floorButtonList = new List<GameObject>();
 
     // Use this for initialization
     void Start () {
@@ -29,11 +43,13 @@ public class TileEditor : MonoBehaviour {
         editorTextureRenderer = editorTexture.GetComponent<SpriteRenderer>();
         editorTextureRenderer.color = new Color(editorTextureRenderer.color.r, editorTextureRenderer.color.g, editorTextureRenderer.color.b, 0.5f);
         //levelManager.loadLevel();
-
+        
         maintiles = levelManager.maintilelist;
         misctiles = levelManager.misctilelist;
         Debug.Log(maintiles.Count);
         Debug.Log(misctiles.Count);
+
+        generateTileEditor();
     }
 	
 	// Update is called once per frame
@@ -47,7 +63,7 @@ public class TileEditor : MonoBehaviour {
         editorTexture.transform.position = new Vector2(ray.x, ray.y);
 
         if (Input.GetMouseButtonDown(0))
-            Debug.Log("left click");
+            Debug.Log("Left click");
 
         if (Input.GetMouseButtonDown(1))
         {
@@ -70,6 +86,31 @@ public class TileEditor : MonoBehaviour {
         */
     }
 
+    void generateTileEditor()
+    {
+        panelPosition = GameObject.Find("Floor_panel").transform.position;
+        editorTileCount = Mathf.FloorToInt(Camera.main.pixelHeight / 32f);
+        Debug.Log(Camera.main.pixelHeight);
+        for (int i = 0; i < editorTileCount; i++)
+        {
+            newTile = Instantiate(menutile, new Vector3(0, -i * 32 - 16), Quaternion.identity) as GameObject;
+            Vector2 pos = new Vector2(0, panelPosition.y + (- i * 32 - 16));
+            newTile.transform.SetParent(GameObject.Find("Floor_panel").transform);
+            newTile.transform.localScale = new Vector3(1, 1, 1);
+            newTile.transform.localPosition = pos;
+            /*
+            SetActiveTile script = newTile.GetComponent<SetActiveTile>();
+            script.index=i;
+            script.list = 0; // 0 = floor, 1 = walls, 2 = misc
+            script.changeSelf(levelManager.Sprite_FloorTileList[tileIndex]); 
+            tileIndex++;
+            */
+            floorButtonList.Add(newTile);
+        }
+        Debug.Log(floorButtonList.Count);
+        populateMenu(floorButtonList, 0);
+    }
+
     void DestroyTile(GameObject obj, List<Level.tile> list, int index)
     {
         try
@@ -80,6 +121,28 @@ public class TileEditor : MonoBehaviour {
         }
         catch (System.Exception) { }
     }
+
+    void populateMenu(List<GameObject> listOfButtons, byte list)
+    {
+        //list 0 for floor, 1 for walls, 2 for misc;
+        for (int i = 0; i < listOfButtons.Count; i++)
+        {
+            SetActiveTile script = listOfButtons[i].GetComponent<SetActiveTile>();
+            script.index = floorTileIndex+i;
+            script.list = list; // 0 = floor, 1 = walls, 2 = misc
+            script.changeSelf(levelManager.Sprite_FloorTileList[floorTileIndex+i]);
+        }
+    }
+
+    public void ChangeActiveTile(int ind, GameObject obj)
+    {
+        activeObjectIndex = ind;
+        activeObject = obj;
+        SpriteRenderer newSprite = obj.GetComponent<SpriteRenderer>();
+        editorTextureRenderer.color = newSprite.color;
+        editorTextureRenderer.sprite = newSprite.sprite;
+    }
+
 
     /*
     GameObject HoverSelect()
@@ -98,4 +161,4 @@ public class TileEditor : MonoBehaviour {
         else return null;
     }
     */
-}
+        }
